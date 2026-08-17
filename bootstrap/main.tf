911ff2a9d5f91b40e86d846dae597b23742d7bc6
Rayyan Biram <rayyanbiram@hotmail.com>
2026-08-17 18:44:33 +0000

    modify s3 permissions to delete objects for state locking

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -128,7 +128,9 @@ resource "aws_iam_role_policy" "s3" {
         "Action" : [
           "s3:GetObject",
           "s3:PutObject",
+          "s3:DeleteObject",
           "s3:ListBucket"
+          
         ],
         "Resource" : [
           "arn:aws:s3:::it-tools-ecs-fargate-terraform-state",
